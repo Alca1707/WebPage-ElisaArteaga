2b4abb6b311dc9c8bff982fb6197b5286b04af4c
Diego Gómez <diego.fernandogc@gmail.com>
2022-01-12 16:46:15 -0600

    email sender

--- contactForm.php
+++ contactForm.php
@@ -14,81 +14,59 @@
                 <p>¿Tienes alguna duda?, ¿Deseas hacer equipo? escríbeme y pongámonos en contacto, será un gusto trabajar contigo y hacer ese sueño realidad.</p>
             </div>
             <div class="col-lg-7 offset-lg-1">
-                <form>
+                <form action="emailsender.php" method="post">
                     <div class="row mb-3">
                         <label for="name" class="col-sm-2 col-form-label">Nombre</label>
                         <div class="col-sm-10">
-                          <input type="password" class="form-control" id="name">
+                          <input type="text" class="form-control" id="name" name="Name">
                         </div>
                     </div>
 
                     <div class="row mb-3">
                       <label for="email" class="col-sm-2 col-form-label">Email</label>
                       <div class="col-sm-10">
-                        <input type="email" class="form-control" id="email">
+                        <input type="email" class="form-control" id="email" name="Email">
                       </div>
                     </div>
                     
                     <div class="row mb-3">
                         <label for="phone" class="col-sm-2 col-form-label">Teléfono</label>
                         <div class="col-sm-10">
-                          <input type="password" class="form-control" id="phone">
+                          <input type="text" class="form-control" id="phone" name="Phone">
                         </div>
                     </div>
 
                     <div class="row mb-3">
                         <label for="inputPassword3" class="col-sm-2 col-form-label">Asunto</label>
                         <div class="col-sm-10">
-                        <input type="password" class="form-control" id="inputPassword3">
+                        <input type="text" class="form-control" id="inputPassword3" name="Subject">
                       </div>
                     </div>
 
                     <div class="row mb-3">
                         <label for="inputPassword3" class="col-sm-3 col-form-label">Método de contacto</label>
-                        <select class="form-select" aria-label="Default select example">
+                        <select class="form-select" aria-label="Default select example" name="Type">
                             <option selected>Email</option>
                             <option value="2">Llamada teléfonica</option>
                             <option value="3">Mensaje por Whatsapp</option>
-                          </select>
+                        </select>
                     </div>
                     <div class="row mb-3">
                         <label for="inputPassword3" class="col-sm-2 col-form-label">Mensaje</label>
-                        <textarea class="form-control" aria-label="With textarea"></textarea>
+                        <textarea class="form-control" aria-label="With textarea" name="Message"></textarea>
                     </div>
 
-                    <button type="submit" class="btn btn-primary">Sign in</button>
+                    <button type="submit" class="btn btn-primary">Enviar</button>
                 </form>
-
-                <!--<form action="">
-                    <label for="name">Nombre</label>
-                    <input id="name" type="text">
-                    <small class="error"></small>
-        
-                    <label for="mail">Email</label>
-                    <input id="mail" type="text">
-                    <small class="error"></small>
-        
-                    <label for="telephone">Teléfono</label>
-                    <input id="telephone" type="text">
-                    <small class="error"></small>
-        
-                    <label for="subject">Asunto</label>
-                    <input id="subject" type="text">
-                    <small class="error"></small>
-        
-                    <label for="message">Mensaje</label>
-                    <textarea name="" id="message" cols="30" rows="10"></textarea>
-                    <small class="error"></small>
-        
-                    <div>
-                        <input type="submit">
-                    </div>
-                </form>-->
             </div>
         </div>  
-        
     </div>
     <?php
         include "footer.html"
     ?>
+    <script>
+        if (location.href.indexOf("#") != -1) {
+            document.getElementById(location.href.slice(location.href.indexOf("#") + 1)).style.display = "inline-block";
+        }
+    </script>
 </html>
---
+++ emailsender.php
@@ -0,0 +1,29 @@
+<?php
+try{
+    $Name = filter_input(INPUT_POST, 'Name', FILTER_SANITIZE_STRING);
+    $Email = filter_input(INPUT_POST, 'Email', FILTER_SANITIZE_STRING);
+    $Phone = filter_input(INPUT_POST, 'Phone', FILTER_SANITIZE_STRING);
+    $Subject = filter_input(INPUT_POST, 'Subject', FILTER_SANITIZE_STRING);
+    $Type = filter_input(INPUT_POST, 'Type', FILTER_SANITIZE_STRING);
+    $Message = filter_input(INPUT_POST, 'Message', FILTER_SANITIZE_STRING);
+    
+    $cabeceras = "From: ". $Name ." <". $Email .">" . "\r\n";
+    
+    $To = "HELLO@ELISAARTEAGA.COM";
+    $Mensaje = $Message;
+    
+    $Mensaje .= "\n\n";
+    $Mensaje .= "Nombre: " . $Name . "\n";
+    $Mensaje .= "Email: " . $Email  . "\n";
+    $Mensaje .= "Teléfono: " . $Phone  . "\n";
+    $Mensaje .= "Método de contacto: " . $Type  . "\n";
+
+    if(!mail($To, $Subject, $Mensaje, $cabeceras)){
+        throw new Exception('Failed');
+    }
+    header('Location: contactForm.php#SENDED');
+}
+catch (Exception $e) {
+    header('Location: contactForm.php#FAILED');
+}
+?>
