6c453ef01f00457a2df5b89dda5f9ac449cf8289
Karen Rivas <karenrivasg17@gmail.com>
2022-01-13 12:30:08 -0600

    Veneno responsive

--- veneno.php
+++ veneno.php
@@ -7,32 +7,33 @@
 
 
     <div class="ImgMainBackground">
+        <img src="img/Veneno_01.jpg" class="fullWidthImg" alt="">
         <div class="col-lg-4 venenoMainText">
-            <img src="img/Veneno-logo.svg" class="col-lg-9" alt="">
-            <p id="venenoText" class="col-lg-9 espaciado80"><br>La naturaleza humana se deja cautivar por su belleza, cae bajo su hechizo embriagada por el artificio de lo que perciben los sentidos. Esta debilidad es ineludible. Es la historia que se cuenta desde el origen de los tiempos.  Rebelde, salvaje y libre, al compás de su propia música va, florece para ser la tentación de quién pose sus ojos sobre ella</p>          
+            <img src="img/Veneno-logo.svg" class="col-lg-9 col-md-5 col-sm-8 col-8" alt="">
+            <p id="venenoText" class="col-lg-9 col-md-9 col-sm-9 col-9 espaciado80"><br>La naturaleza humana se deja cautivar por su belleza, cae bajo su hechizo embriagada por el artificio de lo que perciben los sentidos. Esta debilidad es ineludible. Es la historia que se cuenta desde el origen de los tiempos.  Rebelde, salvaje y libre, al compás de su propia música va, florece para ser la tentación de quién pose sus ojos sobre ella.</p>          
         </div>
-         <img src="img/Veneno_01.jpg" class="fullWidthImg" alt="">
+
     </div>
 
     <img src="img/Veneno_02.jpg" class="fullWidthImg espaciado80" alt="">
     
     <div class="container espaciado80">   
         <div class="row">
-            <img src="img/Veneno_04.jpg" class="col-lg-5" alt="">
-            <p class="col-lg-5 offset-lg-2" id="descriptionVeneno">DESCRIPCIÓN <br>Colección de calzado femenino a partir de la intervención de calzado base. La colección consta de 3 pares de tacones: un par de stilletos y 2 pares de sandalias.</p>
+            <img src="img/Veneno_04.jpg" class="col-lg-5 col-md-6 col-sm-6 col-8" alt="">
+            <p class="col-lg-5 col-md-6 col-sm-6 col-8 offset-lg-2" id="descriptionVeneno">DESCRIPCIÓN <br>Colección de calzado femenino a partir de la intervención de calzado base. La colección consta de 3 pares de tacones: un par de stilletos y 2 pares de sandalias.</p>
         </div>
         <div class="row">
-            <img src="img/Veneno_06-bocetos.jpg" class="col-lg-7 noSpacingImg" alt="">
-            <img src="img/Veneno_05.jpg" class="col-lg-5 noSpacingImg" alt="">
+            <img src="img/Veneno_06-bocetos.jpg" class="col-lg-7 col-md-7 col-sm-7 col-9 noSpacingImg mediaSpacing" alt="">
+            <img src="img/Veneno_05.jpg" class="col-lg-5 col-md-5 col-sm-5 col-8 noSpacingImg" alt="">
         </div>
         
     </div>
 
     <div class="espaciado80 bodyFooter">
         <div class="clearfix">
-            <img src="img/Veneno_03.jpg" class="col-lg-8 float-end" alt="">
-            <div class="col-lg-2 offset-lg-1">
-                <p id="pinkVenenoText">Flores inspiracionales |<br> detalles abstraídos:</p>
+            <img src="img/Veneno_03.jpg" class="col-lg-8 col-md-8 col-sm-11 col-11 float-end mediaSpacingBottom" alt="">
+            <div class="col-lg-2 col-md-2 col-sm-9 col-8 offset-lg-1 offset-md-1 offset-sm-1 offset-1">
+                <p id="pinkVenenoText" >Flores inspiracionales |<br> detalles abstraídos:</p>
                 <p id="venenoTextFooter">a. ‘higuera infernal’:
                 filamentos o púas.<br>
                 <br>b. ‘dedalera’:
