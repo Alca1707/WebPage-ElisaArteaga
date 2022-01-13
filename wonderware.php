a18c3f8751e1c8562f133d1a0bc50eb4ddd8bdc5
Karen Rivas <karenrivasg17@gmail.com>
2022-01-13 14:49:41 -0600

    Header responsive

--- wonderware.php
+++ wonderware.php
@@ -9,20 +9,22 @@
         <img src="img/Wonderware_01.1.jpg" class="fullWidthImg" alt="">
     </div>
     <div class="container espaciado80">
-        <div class="row">
-            <img src="img//Wonderware_logo.svg" class="titlesImgLittle col-lg-5" alt="">
-            <p id="lightBlueText" class="col-lg-4 offset-lg-2">Fantasía y sorpresa, espejismo e ilusión, en un mundo peculiar y extraño nada es lo que parece.</p>
+        <div class="row align-items-center">
+            <img src="img//Wonderware_logo.svg" class="titlesImgLittle col-lg-5 col-md-6 col-sm-8 col-8 mediaSpacingBottom" alt="">
+            <p id="lightBlueText" class="col-lg-4 col-md-5 col-sm-11 col-11 offset-md-1 offset-lg-2">Fantasía y sorpresa, espejismo e ilusión, en un mundo peculiar y extraño nada es lo que parece.</p>
         </div>
     </div>
 
     <div class="espaciado80">
         <div class="clearfix">
-            <img src="img//Wonderware_02.jpg" class="col-lg-8 float-end" alt="">
-            <p class="container col-lg-3 offset-lg-1">Wonderware es un juego de té de 5 piezas, elaboradas en torno con arcilla blanca, con acabados en de bajo relieve y tapaderas de acrílico.<br><br><br>
-                <b>Inspiración:</b><br><br> Personajes de la obra <b>“Alicia en el país de las maravillas”</b>:
-                el gato risón, el conejo blanco y la sota de corazones. El género del “sinsentido” al que pertenece la obra.
-                Jugar con el equilibrio, la forma y el apilamiento. Zoomorfismo de las piezas.
-            </p>
+            <img src="img//Wonderware_02.jpg" class="col-lg-8 col-md-8 col-sm-10 col-10 float-end mediaSpacingBottom" alt="">
+            <div id="alingVertical">
+                <p class="container col-lg-2 col-md-3 col-sm-9 col-10 offset-lg-1 offset-md-1">Wonderware es un juego de té de 5 piezas, elaboradas en torno con arcilla blanca, con acabados en de bajo relieve y tapaderas de acrílico.<br><br><br>
+                    <b>Inspiración:</b><br><br> Personajes de la obra <b>“Alicia en el país de las maravillas”</b>:
+                    el gato risón, el conejo blanco y la sota de corazones. El género del “sinsentido” al que pertenece la obra.
+                    Jugar con el equilibrio, la forma y el apilamiento. Zoomorfismo de las piezas.
+                </p>
+            </div>
         </div>
     </div>
 
@@ -37,10 +39,10 @@
     <div class="container espaciado20">
         <p class="row"><b>piezas:</b></p>
         <div class="row">
-            <p class="col-lg-6"><b>la vajilla consiste en:</b><br>
+            <p class="col-lg-6 col-md-6 noMarginBottom"><b>la vajilla consiste en:</b><br>
                 - taza / cup - (200 ml) / 12 x 6 x 12 cm<br>
                 - tetera / teapot - (500 ml / 1.5 lt) / 12 x 23 x 12 cm</p>
-            <p class="col-lg-6">
+            <p class="col-lg-6 col-md-6 noMarginBottom">
                 - depósito para leche / milk pot - (150 ml) / 8 x 9 x 8 cm<br>
                 - azucarera / sugar bowl -  (x ml) / 9 x 10 x 9 cm<br>
                 - cuchara / spoon - (5 ml) / 3 x 14 x 1 cm</p>
