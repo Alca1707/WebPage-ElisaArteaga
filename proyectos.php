c7e218efc4fb3307835ce64c1d2aa06c1f89fd94
Diego Gómez <diego.fernandogc@gmail.com>
2022-01-13 21:34:19 -0600

    img hover title for projects

--- proyectos.php
+++ proyectos.php
@@ -23,18 +23,36 @@
         <div id="projects">
             <div class="row rowProyectos">
                 <a href="tlaloquines.php" class="col-lg-3 col-md-3 col-sm-3 col-3 p-2">
+                    <img src="img/Tlaloquines-hover.png"  alt="">
                     <img src="img/Proyectos_01.jpg" alt="">
-                    <img src="img/Tlaloquines-hover.png" class="imgHover" alt="">
                 </a>
-                <a href="wonderware.php" class="col-lg-3 col-md-3 col-sm-3 col-3 p-2"><img src="img/Proyectos_03.jpg"  alt=""></a>
-                <a href="matina.php" class="col-lg-3 col-md-3 col-sm-3 col-3 p-2"><img src="img/Proyectos_04.jpg"  alt=""></a>
-                <a href="eden.php" class="col-lg-3 col-md-3 col-sm-3 col-3 p-2"><img src="img/Proyectos_05.jpg"  alt=""></a>
+                <a href="wonderware.php" class="col-lg-3 col-md-3 col-sm-3 col-3 p-2">
+                    <img src="img/Tlaloquines-hover.png" alt="">
+                    <img src="img/Proyectos_03.jpg"  alt="">
+                </a>
+                <a href="matina.php" class="col-lg-3 col-md-3 col-sm-3 col-3 p-2">
+                    <img src="img/Tlaloquines-hover.png" alt="">
+                    <img src="img/Proyectos_04.jpg"  alt="">
+                </a>
+                <a href="eden.php" class="col-lg-3 col-md-3 col-sm-3 col-3 p-2">
+                    <img src="img/Tlaloquines-hover.png" alt="">
+                    <img src="img/Proyectos_05.jpg"  alt="">
+                </a>
             </div>
     
             <div class="row rowProyectos justify-content-center">
-                <a href="pasarela.php" class="col-lg-3 col-md-3 col-sm-3 col-3 p-2"><img src="img/Proyectos_06.jpg" alt=""></a>
-                <a href="veneno.php" class="col-lg-3 col-md-3 col-sm-3 col-3 p-2"><img src="img/Proyectos_07.jpg" alt=""></a>
-                <a href="pulse.php" class="col-lg-3 col-md-3 col-sm-3 col-3 p-2"><img src="img/Proyectos_08.jpg" alt=""></a>
+                <a href="pasarela.php" class="col-lg-3 col-md-3 col-sm-3 col-3 p-2">
+                    <img src="img/Tlaloquines-hover.png" alt="">
+                    <img src="img/Proyectos_06.jpg" alt="">
+                </a>
+                <a href="veneno.php" class="col-lg-3 col-md-3 col-sm-3 col-3 p-2">
+                    <img src="img/Tlaloquines-hover.png" alt="">
+                    <img src="img/Proyectos_07.jpg" alt="">
+                </a>
+                <a href="pulse.php" class="col-lg-3 col-md-3 col-sm-3 col-3 p-2">
+                    <img src="img/Tlaloquines-hover.png" alt="">
+                    <img src="img/Proyectos_08.jpg" alt="">
+                </a>
             </div>
         </div>
 
